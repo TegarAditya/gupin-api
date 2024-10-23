cdc4108fc580e28fcaf35cf30ecabb2b2aab48e4
Tegar Aditya <tegaraditya53@gmail.com>
2024-10-23 09:42:25 +0700

    Refactor index.ts to add new routes and middleware

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
-import { serve } from "@hono/node-server"
 import { Hono } from "hono"
-import api from "./routes/api"
 import { logger } from "hono/logger"
 import { cors } from "hono/cors"
+import { secureHeaders } from "hono/secure-headers"
+import { compress } from "hono/compress"
+import { serve } from "@hono/node-server"
+import { serveStatic } from "@hono/node-server/serve-static"
+import { trimTrailingSlash } from "hono/trailing-slash"
+import docs from "./routes/docs"
+import api from "./routes/api"
+import healthcheck from "./routes/healthcheck"
 
 const app = new Hono()
 
 app.use(logger())
-app.use(cors())
+    .use(cors())
+    .use(secureHeaders())
+    .use(trimTrailingSlash())
+    .use(compress())
 
 app.route("/api", api)
+app.route("/docs", docs)
+app.route("/healthcheck", healthcheck)
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3033
+app.use("/*", serveStatic({ root: "./public/" }))
+
+const port = process.env.PORT ? Number(process.env.PORT) : 3000
 console.log(`Server is running on port ${port}`)
 
 serve({
