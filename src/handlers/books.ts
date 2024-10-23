d92c5601631d535034f3c6aa9051204e180ba9ce
Tegar Aditya <tegaraditya53@gmail.com>
2024-10-23 09:41:40 +0700

    Refactor books.ts handler to add new API endpoints and improve search functionality

--- src/handlers/books.ts
+++ src/handlers/books.ts
@@ -1,19 +1,53 @@
 import { createFactory } from "hono/factory"
 import db from "../plugins/database"
+import { zValidator } from "@hono/zod-validator"
+import { z } from "zod"
+import { Prisma } from "@prisma/client"
 
 const factory = createFactory()
 
-export const getBooks = factory.createHandlers(async (c) => {
-    const books = await db.bukunontext.findMany({
-        where: {
-            NOT: [{ pdf: { contains: "tim.bupin.id" } }],
-        },
-        select: {
-            judul: true,
-            isi: true,
-            pdf: true,
-            cover: true,
-        },
-    })
-    return c.json(books, 200)
-})
+const jenjangEnum = z.enum(["SD", "SMP", "SMA", "SMK", "MI", "MTS", "MA"])
+
+// GET /api/books
+export const getBooks = factory.createHandlers(
+    zValidator(
+        "query",
+        z.object({
+            jenjang: z.union([jenjangEnum, z.array(jenjangEnum)]).optional(),
+            search: z.string().optional(),
+        })
+    ),
+    async (c) => {
+        const jenjangQuery = c.req.queries("jenjang") || c.req.query("jenjang")
+        const whereClause: Prisma.bukunontextWhereInput = {}
+
+        if (jenjangQuery) {
+            whereClause.judul = {
+                contains: Array.isArray(jenjangQuery)
+                    ? jenjangQuery.join(",")
+                    : jenjangQuery,
+            }
+        }
+
+        const books = await db.bukunontext.findMany({
+            where: {
+                AND: [
+                    { pdf: { not: { contains: "tim.bupin.id" } } },
+                    { judul: { contains: c.req.query("search") || undefined } },
+                    { ...whereClause },
+                ],
+            },
+            select: {
+                judul: true,
+                isi: true,
+                pdf: true,
+                cover: true,
+            },
+            distinct: ["pdf"],
+            orderBy: {
+                judul: "asc",
+            },
+        })
+        return c.json(books, 200)
+    }
+)
