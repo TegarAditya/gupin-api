3b3f7a16171814b5d6ddd69e90ab46384364fc4b
Tegar Aditya <tegaraditya53@gmail.com>
2024-11-28 09:10:22 +0700

    Refactor getBooks handler to support multiple search queries and improve condition creation

--- src/handlers/books.ts
+++ src/handlers/books.ts
@@ -2,11 +2,19 @@ import { createFactory } from "hono/factory"
 import db from "../plugins/database"
 import { zValidator } from "@hono/zod-validator"
 import { z } from "zod"
-import { Prisma } from "@prisma/client"
 
 const factory = createFactory()
-
 const jenjangEnum = z.enum(["SD", "SMP", "SMA", "SMK", "MI", "MTS", "MA"])
+
+/**
+ * Creates an array of condition objects based on the provided query and field.
+ */
+const createConditions = (query: string | string[], field: string) => {
+    const values = Array.isArray(query) ? query : [query].filter(Boolean)
+    return values.map((value) => ({
+        [field]: { contains: value },
+    }))
+}
 
 // GET /api/books
 export const getBooks = factory.createHandlers(
@@ -14,27 +22,30 @@ export const getBooks = factory.createHandlers(
         "query",
         z.object({
             jenjang: z.union([jenjangEnum, z.array(jenjangEnum)]).optional(),
-            search: z.string().optional(),
+            search: z.union([z.string(), z.array(z.string())]).optional(),
         })
     ),
     async (c) => {
         const jenjangQuery = c.req.queries("jenjang") || c.req.query("jenjang")
-        const whereClause: Prisma.bukunontextWhereInput = {}
+        const searchQuery = c.req.queries("search") || c.req.query("search")
+
+        const conditions = []
 
         if (jenjangQuery) {
-            whereClause.judul = {
-                contains: Array.isArray(jenjangQuery)
-                    ? jenjangQuery.join(",")
-                    : jenjangQuery,
-            }
+            conditions.push({ OR: createConditions(jenjangQuery, "judul") })
         }
+
+        if (searchQuery) {
+            conditions.push({ OR: createConditions(searchQuery, "judul") })
+        }
+
+        const whereClause = conditions.length ? { AND: conditions } : {}
 
         const books = await db.bukunontext.findMany({
             where: {
                 AND: [
                     { pdf: { not: { contains: "tim.bupin.id" } } },
-                    { judul: { contains: c.req.query("search") || undefined } },
-                    { ...whereClause },
+                    whereClause,
                 ],
             },
             select: {
@@ -48,6 +59,7 @@ export const getBooks = factory.createHandlers(
                 judul: "asc",
             },
         })
+
         return c.json(books, 200)
     }
 )
